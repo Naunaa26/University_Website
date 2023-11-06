5ece7170796723fb969e65f019d31d6079bc9b16
Naunaa26 <bluenaufal7@gmail.com>
2023-11-06 13:32:26 +0700

    Website_Clone

---
+++ from-handler.php
@@ -0,0 +1,24 @@
+<?php
+
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$email_from = 'bluenaufal7@gmail.com'; // Sesuaikan dengan alamat email situs web Anda
+
+$email_subject = 'New Form Submission';
+
+$email_body = "User Name: $name.\n" .
+    "User Email: $visitor_email.\n" .
+    "Subject: $subject.\n" .
+    "User Massage: $message.\n";
+
+$to = 'bluenaufal7@gmail.com';
+
+$headers = "From: $email_from \r\n";
+$headers .= "Reply-To: $visitor_email \r\n";
+
+mail($to, $email_subject, $email_body, $headers);
+
+header("Location: index.html");
